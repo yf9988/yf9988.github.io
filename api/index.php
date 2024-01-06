b96309dd7b3ea1c4f22dd292d782a37d87bb6a93
yf9988 <yufeng1688778@gmail.com>
2024-01-06 13:15:53 +0800

    01.06

--- api/index.php
+++ api/index.php
@@ -1,37 +1,28 @@
 <?php
-/*
-*代码来自omii.top
-*/
-//存放api随机图链接的文件名img.txt
+//存有美图链接的文件名img.txt
 $filename = "img.txt";
 if(!file_exists($filename)){
-    die('文件不存在');
+   die('文件不存在');
 }
- 
 //从文本获取链接
 $pics = [];
 $fs = fopen($filename, "r");
 while(!feof($fs)){
-    $line=trim(fgets($fs));
-    if($line!='' && substr($str , 0 , 1) != '#'){
-        array_push($pics, $line);
-    }
+   $line=trim(fgets($fs));
+   if($line!=''){
+       array_push($pics, $line);
+   }
 }
- 
-// 从数组随机获取链接
-$pic = $pics[random_int(0, count($pics) - 1)]; 
- 
+//从数组随机获取链接
+$pic = $pics[array_rand($pics)];
 //返回指定格式
 $type=$_GET['type'];
 switch($type){
- 
 //JSON返回
 case 'json':
-    header('Content-type:text/json');
-    die(json_encode(['pic'=>$pic]));
- 
+   header('Content-type:text/json');
+   die(json_encode(['pic'=>$pic]));
 default:
-    die(header("Location: $pic"));
-}
- 
+   die(header("Location: $pic"));
+} 
 ?>
